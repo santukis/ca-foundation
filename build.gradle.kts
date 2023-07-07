f5888c144daf9571961f708e1e415189ea7ded91
David <santamadavid@gmail.com>
2023-07-07 14:30:41 +0200

    CA-01 main structure

---
+++ build.gradle.kts
@@ -0,0 +1,7 @@
+plugins {
+    id("com.android.application").version("8.0.2").apply(false)
+    id("com.android.library").version("8.0.2").apply(false)
+    id("org.jetbrains.kotlin.android").version("1.8.22").apply(false)
+    id("org.jetbrains.kotlin.jvm").version("1.8.22").apply(false)
+    id("io.gitlab.arturbosch.detekt") version "1.23.0" apply false
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,36 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("gradle/configuration/dependencies/libs.versions.toml"))
+        }
+    }
+}
+
+rootProject.name = "ca-foundation"
+include(":sample:app")
+include(":sample:featureA")
+include(":sample:featureB")
+include(":sample:featureC")
+include(":sample:featureD")
+
+include(":domain:usecase:input")
+include(":domain:usecase:output")
+
+include(":ui:navigation")
+include(":ui:theme")
+
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")

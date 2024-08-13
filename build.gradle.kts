19c200c89557ad3e92eeef00693c08fd131e842e
santukis <santamadavid@gmail.com>
2024-08-13 10:43:18 +0200

    Updated ksp version

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,5 @@ plugins {
     id("org.jetbrains.kotlin.android").version("1.9.25").apply(false)
     id("org.jetbrains.kotlin.jvm").version("1.9.25").apply(false)
     id("io.gitlab.arturbosch.detekt").version("1.23.0").apply(false)
-    id("com.google.devtools.ksp").version("1.8.22-1.0.11").apply(false)
+    id("com.google.devtools.ksp").version("1.9.25-1.0.20").apply(false)
 }
